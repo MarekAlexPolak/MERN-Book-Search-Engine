82c17e5f1f603a162ae190c28b90fb63e57f5eb1
Marek Polak <markalexpolak@gmail.com>
2023-08-01 19:49:30 -0400

    updated server.js to use apollo server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const {authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,8 +15,28 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/'));
+})
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+
+// Apollo Server configuration
+const server = new ApolloServer({
+  typeDefs, 
+  resolvers, 
+  context: authMiddleware,
 });
+
+const startServ = async () => {
+  await server.start();
+  server.applyMiddleware({app});
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
+}
+
+startServ();
+
+
+
+
